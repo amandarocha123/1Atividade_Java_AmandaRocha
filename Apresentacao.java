a96a7089f2c9b2b8b0a9fa86f1d439d2e554dbd2
amandarocha123 <amanda403n.u@gmail.com>
2025-10-24 10:38:07 -0300

    Add files via upload

---
+++ Apresentacao.java
@@ -0,0 +1,7 @@
+public class Apresentacao {
+    public static void main(String[] args) {
+        System.out.print("Nome completo: Amanda Rocha\n");
+        System.out.print("Idade: 16 anos\n");
+        System.out.print("Cidade: Acopiara-ce\n");
+    }
+}
---
+++ FraseBonita.java
@@ -0,0 +1,6 @@
+public class FraseBonita {
+    public static void main(String[] args) {
+        // Imprime uma frase inspiradora contendo aspas internas
+        System.out.print("\"A persistência realiza o impossível.\" — Autor Desconhecido");
+    }
+}
